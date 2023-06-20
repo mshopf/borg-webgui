d70a03a298189ab861dbd5742397e8db12f8f553
Matthias Hopf <mat@mshopf.de>
2023-06-20 16:25:20 +0200

    Default path + title

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     //httpsPort: 8443,
     //data: [ { file: '*-data-tree.bin', restore: '/space/nobackup/_RESTORE', borg_args: ['--sparse', '--strip-components', '2'] } ],
     borg_repo: '/space/backup/borg',
-    data: [ { file: 'borg-backup-data-tree.bin', restore: '/space/nobackup/_RESTORE', borg_args: ['--sparse'] } ],
+    data: [ { file: 'data/borg-backup-data-tree.bin', restore: '/space/nobackup/_RESTORE', borg_args: ['--sparse'] } ],
     auth: {
         'admin': { pwd: '$argon2id$v=19$m=65536,t=3,p=4$pxZKX/+NxOaln4c8Z7xzXg$OhJ+PjiPKmzqNAblkr1++VEfhR6W/6h8dUiH+BjCK/8', },   // admin/admin
     },
