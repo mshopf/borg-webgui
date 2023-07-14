e07feb4f17f3b6f846f058fef0b1ac10d5339314
Matthias Hopf <mat@mshopf.de>
2023-07-14 15:43:08 +0200

    Tests adapted to new syntax.

--- test.sh
+++ test.sh
@@ -1,60 +1,119 @@
-#!/bin/bash -xe
-node ./tree.js - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_1234_direct.json
+#!/bin/bash -e
+
+rm -f borg-backup-data-tree.bin m4-data-tree.bin m2-?-data-tree.bin i2-?-data-tree.bin m3-?-data-tree.bin i3-?-data-tree.bin
+
+node ./tree.js -m - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2
+mv -f borg-backup-data-tree.bin m4-data-tree.bin
 
 if [ "x$1" = x1 -o "x$1" = x ] ; then
-node ./tree.js - +data/zuse2-%-2022-01-31-023001.bz2 >z_1.json
-node ./tree.js ./z_1.json +data/zuse2-%-2023-01-31-023001.bz2 >z_12.json
-node ./tree.js ./z_12.json +data/zuse2-%-2023-05-27-023001.bz2 >z_123.json
-node ./tree.js ./z_123.json +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_1234.json
-echo "diff 1"
-diff -uq z_1234{,_direct}.json
-rm z_1.json z_12.json z_123.json z_1234.json
+
+    node ./tree.js -i -                         +data/zuse2-%-2022-01-31-023001.bz2
+    node ./tree.js -i borg-backup-data-tree.bin +data/zuse2-%-2023-01-31-023001.bz2
+    node ./tree.js -i borg-backup-data-tree.bin +data/zuse2-%-2023-05-27-023001.bz2
+    node ./tree.js -i borg-backup-data-tree.bin +data/zuse2-%-2023-05-28-023001.bz2
+    # re-read and dump to sort entries in same maner as in-memory creation
+    node ./tree.js -i borg-backup-data-tree.bin
+
+    echo diff 1
+    cmp m4-data-tree.bin borg-backup-data-tree.bin || exit 1
+
+    rm -f borg-backup-data-tree.bin
+
 fi
 
 if [ "x$1" = x2 -o "x$1" = x ] ; then
-node ./tree.js ./z_1234_direct.json -2022-01-31-023001 -2023-01-31-023001 | jq -S . >z_1234_12.json
-node ./tree.js ./z_1234_direct.json -2023-01-31-023001 -2022-01-31-023001 | jq -S . >z_1234_21.json
-echo "diff 2a"
-diff -uq z_1234_12.json z_1234_21.json
-node ./tree.js ./z_1234_direct.json -2022-01-31-023001 -2023-05-27-023001 | jq -S . >z_1234_13.json
-node ./tree.js ./z_1234_direct.json -2023-05-27-023001 -2022-01-31-023001 | jq -S . >z_1234_31.json
-echo "diff 2b"
-diff -uq z_1234_13.json z_1234_31.json
-node ./tree.js ./z_1234_direct.json -2022-01-31-023001 -2023-05-28-023001 | jq -S . >z_1234_14.json
-node ./tree.js ./z_1234_direct.json -2023-05-28-023001 -2022-01-31-023001 | jq -S . >z_1234_41.json
-echo "diff 2c"
-diff -uq z_1234_14.json z_1234_41.json
+
+    ln m4-data-tree.bin m2-1-data-tree.bin
+    ln m4-data-tree.bin m2-2-data-tree.bin
+    ln m4-data-tree.bin i2-3-data-tree.bin
+    ln m4-data-tree.bin i2-4-data-tree.bin
+    node ./tree.js -m m2-1-data-tree.bin -2022-01-31-023001 -2023-01-31-023001
+    node ./tree.js -m m2-2-data-tree.bin -2023-01-31-023001 -2022-01-31-023001
+
+    echo diff 2a
+    cmp m2-1-data-tree.bin m2-2-data-tree.bin || exit 1
+
+    node ./tree.js -i i2-3-data-tree.bin -2022-01-31-023001
+    node ./tree.js -i i2-3-data-tree.bin -2023-01-31-023001
+    node ./tree.js -i i2-4-data-tree.bin -2023-01-31-023001
+    node ./tree.js -i i2-4-data-tree.bin -2022-01-31-023001
+
+    echo diff 2b
+    cmp i2-3-data-tree.bin i2-4-data-tree.bin || exit 1
+
+    echo diff 2c
+    cmp i2-3-data-tree.bin m2-1-data-tree.bin || exit 1
+
+    rm -f m2-?-data-tree.bin i2-?-data-tree.bin
+    ln m4-data-tree.bin m2-1-data-tree.bin
+    ln m4-data-tree.bin i2-3-data-tree.bin
+    ln m4-data-tree.bin i2-4-data-tree.bin
+
+    node ./tree.js -m m2-1-data-tree.bin -2023-05-27-023001 -2023-05-28-023001
+    node ./tree.js -i i2-3-data-tree.bin -2023-05-27-023001
+    node ./tree.js -i i2-3-data-tree.bin -2023-05-28-023001
+    node ./tree.js -i i2-4-data-tree.bin -2023-05-28-023001
+    node ./tree.js -i i2-4-data-tree.bin -2023-05-27-023001
+
+    echo diff 2d
+    cmp i2-3-data-tree.bin i2-4-data-tree.bin || exit 1
+
+    echo diff 2e
+    cmp i2-3-data-tree.bin m2-1-data-tree.bin || exit 1
+
+    rm -f m2-?-data-tree.bin i2-?-data-tree.bin
 fi
 
-node ./tree.js - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 | jq -S . >z_123_direct.json
-node ./tree.js - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_134_direct.json
 if [ "x$1" = x3 -o "x$1" = x ] ; then
-node ./tree.js ./z_123_direct.json +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_1234.json
-echo "diff 3a"
-diff -uq z_1234{,_direct}.json
-node ./tree.js ./z_134_direct.json -2023-05-27-023001 -2023-05-28-023001 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_1234.json
-echo "diff 3b"
-#diff -uq z_1234{,_direct}.json
+
+    ln m4-data-tree.bin i3-0-data-tree.bin
+    ln m4-data-tree.bin i3-1-data-tree.bin
+    ln m4-data-tree.bin i3-2-data-tree.bin
+    ln m4-data-tree.bin i3-3-data-tree.bin
+    ln m4-data-tree.bin i3-4-data-tree.bin
+
+    node ./tree.js -i i3-0-data-tree.bin -2023-05-28-023001
+    node ./tree.js -i i3-0-data-tree.bin +data/zuse2-%-2023-05-28-023001.bz2
+    # re-read and dump to sort entries in same maner as in-memory creation
+    node ./tree.js -i i3-0-data-tree.bin
+
+    echo diff 3x
+    cmp m4-data-tree.bin i3-0-data-tree.bin || exit 1
+
+    # ATM this fails - entries are never fully removed
+    node ./tree.js -m - +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2
+    mv -f borg-backup-data-tree.bin m3-1-data-tree.bin
+    node ./tree.js -i i3-1-data-tree.bin -2022-01-31-023001
+    node ./tree.js -i i3-1-data-tree.bin +data/zuse2-%-2022-01-31-023001.bz2
+
+    echo diff 3a
+    cmp m3-1-data-tree.bin i3-1-data-tree.bin || exit 1
+
+    node ./tree.js -m - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2
+    mv -f borg-backup-data-tree.bin m3-2-data-tree.bin
+    node ./tree.js -i i3-2-data-tree.bin -2023-01-31-023001
+
+    echo diff 3b
+    cmp m3-2-data-tree.bin i3-2-data-tree.bin || exit 1
+
+    node ./tree.js -m - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2
+    mv -f borg-backup-data-tree.bin m3-3-data-tree.bin
+    node ./tree.js -i i3-3-data-tree.bin -2023-05-27-023001
+
+    echo diff 3c
+    cmp m3-3-data-tree.bin i3-3-data-tree.bin || exit 1
+
+    node ./tree.js -m - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2
+    mv -f borg-backup-data-tree.bin m3-4-data-tree.bin
+    node ./tree.js -i i3-4-data-tree.bin -2023-05-28-023001
+
+    echo diff 3d
+    cmp m3-4-data-tree.bin i3-4-data-tree.bin || exit 1
+
 fi
 
-if [ "x$1" = x4 -o "x$1" = x ] ; then
-node ./tree.js - +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-27-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_234_direct.json
-node ./tree.js - +data/zuse2-%-2022-01-31-023001.bz2 +data/zuse2-%-2023-01-31-023001.bz2 +data/zuse2-%-2023-05-28-023001.bz2 | jq -S . >z_124_direct.json
-node ./tree.js ./z_1234_direct.json -2022-01-31-023001 | jq -S . >z_234.json
-node ./tree.js ./z_1234_direct.json -2023-01-31-023001 | jq -S . >z_134.json
-node ./tree.js ./z_1234_direct.json -2023-05-27-023001 | jq -S . >z_124.json
-node ./tree.js ./z_1234_direct.json -2023-05-28-023001 | jq -S . >z_123.json
-# not 100% identical
-# z_234 has some entries without(!) any archives left compared to z_234_direct
-echo "diff 4a"
-diff -uq z_234{,_direct}.json
-echo "diff 4b"
-diff -uq z_134{,_direct}.json
-echo "diff 4c"
-diff -uq z_124{,_direct}.json
-echo "diff 4d"
-diff -uq z_123{,_direct}.json
-fi
+rm -f borg-backup-data-tree.bin m4-data-tree.bin m2-?-data-tree.bin i2-?-data-tree.bin m3-?-data-tree.bin i3-?-data-tree.bin
 
-rm -f z_*.json
+echo "test finished successfully"
 exit 0
+
